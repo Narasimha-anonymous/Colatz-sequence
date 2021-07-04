0f5577f50ad7bb53f1cb95da678479a105b981bb
Narasimha-anonymous <86682921+Narasimha-anonymous@users.noreply.github.com>
2021-07-04 16:31:36 +0530

    Update colatz sequence.py

--- colatz sequence.py
+++ colatz sequence.py
@@ -1,8 +1,12 @@
 n=int(input("Enter a num between 2 and 10^8: "))
+a=0
 while n!=1:
 	print(n,end=" ")
 	if n%2==0:
 		n//=2
+		a+=1
 	else:
 		n=(3*n)+1
+		a+=1
 print(1)
+print("No. of steps taken to reach 1: ",a)
